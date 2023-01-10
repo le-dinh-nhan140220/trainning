b387cd5814050508620c932b795bd8a1a0becfae
le-dinh-nhan140220 <100583242+le-dinh-nhan140220@users.noreply.github.com>
2023-01-10 15:28:18 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+// Bai1
+// function chuvihcn(a, b) {
+//     const chieudai = prompt("Nhap chieu dai");
+//     const chieurong = prompt("Nhap chieu rong");
+//     const result = (+chieudai + +chieurong) * 2
+
+//     alert(result)
+// }
+// chuvihcn()
+
+// function dientichhcn(a, b) {
+//     const chieudai = prompt("Nhap chieu dai");
+//     const chieurong = prompt("Nhap chieu rong");
+//     const result = +chieudai * +chieurong
+
+//     alert(result)
+// }
+// chuvihcn()
+
+// bai2
+// function chuvihtron() {
+//     const bankinh = prompt("Nhap ban kinh R")
+//     const C = 2 * Math.PI * +bankinh
+
+//     alert(C)
+
+// }
+// chuvihtron()
+
+
+
+// bai3
+// function tinhtong(a, b) {
+//     const a = prompt("Nhap so a")
+//     const b = prompt("Nhap so b")
+//     result = +a + +b;
+//     alert(result)
+// }
+// tinhtong()
+
+// function tinhhieu(a, b) {
+//     const a = prompt("Nhap so a")
+//     const b = prompt("Nhap so b")
+//     result = +a - +b;
+//     alert(result)
+// }
+// tinhhieu()
+
+
+// function tinhthuong(a, b) {
+//     const a = prompt("Nhap so a")
+//     const b = prompt("Nhap so b")
+//     result = +a / +b;
+//     alert(result)
+// }
+// tinhthuong()
+
+// function tinhtich(a, b) {
+//     const a = prompt("Nhap so a")
+//     const b = prompt("Nhap so b")
+//     result = +a * +b;
+//     alert(result)
+// }
+// tinhtich()
+
+// // bai4
+// function giaiptbacnhat(a, b)
+// {
+//     if (a == 0 && b == 0){
+//         alert('Phương trình vô số nghiệm');
+//     }
+//     else if (a != 0 && b == 0){
+//         alert('Phương trình có nghiệm x = 0');
+//     }
+//     else if (a == 0 && b != 0){
+//         alert("Phương trình vô nghiệm");
+//     }
+//     else {
+//         alert('Phương trình có nghiệm x = ' + (-b/a));
+//     }
+// }
